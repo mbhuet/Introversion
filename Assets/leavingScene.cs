8c4b5a07dddd1e6c1bd0bf66a01635450fa324bd
Dhruv Karunakaran <dhruvn@gmail.com>
2014-12-03 19:51:38 -0500

    Fixed intro scene

--- Assets/leavingScene.cs
+++ Assets/leavingScene.cs
@@ -4,21 +4,30 @@ using System.Collections;
 public class leavingScene : MonoBehaviour {
 
 
-	bool a1,a2,a3,a4 = false;
+	bool a1,a2,a3,a4,a5 = false;
 	public TextMesh text;
+	public TextMesh text2;
 	// Use this for initialization
 	void Start () {
 		text.text = "";
+		text2.text = "";	
 		StartCoroutine (Wait1(1));
 		StartCoroutine (Wait2(4));
 		StartCoroutine (Wait3(8));
+		StartCoroutine (Wait4(12));
+		StartCoroutine (Wait5(16));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(a1) text.text = "This was fun.";
 		if(a2) text.text = "See you guys at ~@#&*’s birthday party!";
-		if(a3) Application.LoadLevel(1);
+		if (a3) {
+			text.text = "";
+			text2.text = "Sorry I don't think we'll be able to come.";
+				}
+		if(a4) text2.text = "You should still go and meet new people!";
+		if(a5) Application.LoadLevel(1);
 	}
 
 
@@ -37,5 +46,16 @@ public class leavingScene : MonoBehaviour {
 		
 		yield return new WaitForSeconds(seconds);
 		a3 = true;
+	}
+
+	IEnumerator Wait4(float seconds){
+		
+		yield return new WaitForSeconds(seconds);
+		a4 = true;
+	}
+	IEnumerator Wait5(float seconds){
+		
+		yield return new WaitForSeconds(seconds);
+		a5 = true;
 	}
 }
